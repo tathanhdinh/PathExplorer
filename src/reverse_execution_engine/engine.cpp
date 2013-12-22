7f1e08d249d9b2b5ea2157df08d42b34e96a87a1
Ta Thanh Dinh <thanhdinh.ta@inria.fr>
2013-12-22 11:47:22 +0100

    Move forward semantics.

--- src/reverse_execution_engine/engine.cpp
+++ src/reverse_execution_engine/engine.cpp
@@ -39,12 +39,12 @@ extern boost::unordered_map<ADDRINT,
  */
 void engine::move_backward(boost::shared_ptr<checkpoint>& target_checkpoint)
 {
-  // restore the explored trace: because the instruction will be re-executed, the last instruction 
+  // update the explored trace: because the instruction will be re-executed, the last instruction 
   // in the trace must be removed.
   explored_trace = target_checkpoint->trace;
   explored_trace.pop_back();
   
-  // restore the logged values of the written addresses
+  // update the logged values of the written addresses
   boost::unordered_map<ADDRINT, UINT8>::iterator mem_iter = target_checkpoint->memory_log.begin();
   for (; mem_iter != target_checkpoint->memory_log.end(); ++mem_iter) 
   {
@@ -53,15 +53,33 @@ void engine::move_backward(boost::shared_ptr<checkpoint>& target_checkpoint)
   // then clear the set of logged values
   target_checkpoint->memory_log.clear();
   
-  // restore the global memory state
+  // update the global memory state
   boost::unordered_map<ADDRINT, 
                        boost::compressed_pair<UINT8, UINT8>
                        >::iterator global_mem_iter = global_memory_state.begin();
   boost::unordered_map<ADDRINT, 
                        boost::compressed_pair<UINT8, UINT8>
-                       >::iterator local_mem_iter = target_checkpoint->local_memory_state.begin();
+                       >::iterator local_mem_iter;
+  for (; global_mem_iter != global_memory_state.end(); ++global_mem_iter) 
+  {
+    // verify if an element in the global state is also an element in the local state
+    local_mem_iter = target_checkpoint->local_memory_state.find(global_mem_iter->first);
+    if (local_mem_iter != target_checkpoint->local_memory_state.end()) 
+    {
+      // if it is, then the memory value at its address needs to be restored by the last value in 
+      // the local state
+      *(reinterpret_cast<UINT8*>(global_mem_iter->first)) = local_mem_iter->second.second();
+    }
+    else 
+    {
+      // if it is not, then the memory value at its address needs to be restored to the original 
+      // value in the global state
+      *(reinterpret_cast<UINT8*>(global_mem_iter->first)) = global_mem_iter->second.first();
+    }
+  }
+  global_memory_state = target_checkpoint->local_memory_state;
   
-  // restore the cpu context: the instruction (pointed by the IP register in the cpu context) will 
+  // update the cpu context: the instruction (pointed by the IP register in the cpu context) will 
   // be re-executed.
   PIN_ExecuteAt(&(target_checkpoint->cpu_context));
 
@@ -78,20 +96,24 @@ void engine::move_backward(boost::shared_ptr<checkpoint>& target_checkpoint)
  */
 void engine::move_forward(boost::shared_ptr<checkpoint>& target_checkpoint)
 {
-  // restore the explored trace: because the instruction will be re-executed, the last instruction 
+  // update the explored trace: because the instruction will be re-executed, the last instruction 
   // in the trace must be removed.
   explored_trace = target_checkpoint->trace;
   explored_trace.pop_back();
   
-  // restore the total memory state
-//   boost::unordered_map<ADDRINT, 
-//                        boost::compressed_pair<UINT8, UINT8>
-//                        >::iterator mem_iter = target_checkpoint->local_memory_state.begin();
-//   for (; mem_iter != target_checkpoint->local_memory_state.end(); ++mem_iter)
-//   {
-//     *(reinterpret_cast<UINT8*>(mem_iter->first)) = mem_iter->second;
-//   }
-//   
+  // update the glocal memory state
+  boost::unordered_map<ADDRINT, 
+                       boost::compressed_pair<UINT8, UINT8>
+                       >::iterator global_mem_iter = global_memory_state.begin();
+  boost::unordered_map<ADDRINT, 
+                       boost::compressed_pair<UINT8, UINT8>
+                       >::iterator local_mem_iter;
+  boost::unordered_map<ADDRINT, UINT8>::iterator mem_iter;
+  for (; global_mem_iter != global_memory_state.end(); ++global_mem_iter) 
+  {
+    // verify if an element in the global memory state is also an element 
+  }
+    
   // restore the cpu context
   PIN_ExecuteAt(&(target_checkpoint->cpu_context));
   
