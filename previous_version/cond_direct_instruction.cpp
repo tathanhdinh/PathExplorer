c138c9c207affa595bed0ea5653de97b947e26fa
Ta Thanh Dinh <thanhdinh.ta@inria.fr>
2014-02-13 00:29:07 +0100

    before rename folder

--- previous_version/cond_direct_instruction.cpp
+++ previous_version/cond_direct_instruction.cpp
@@ -2,4 +2,11 @@
 
 cond_direct_instruction::cond_direct_instruction(const INS& ins) : instruction(ins)
 {
+  this->is_resolved = false;
+  this->is_bypassed = false;
+  this->is_explored = false;
+
+  this->input_dep_addrs.clear();
+  this->inputs.clear();
+  this->checkpoints.clear();
 }
--- previous_version/cond_direct_instruction.h
+++ previous_version/cond_direct_instruction.h
@@ -2,9 +2,24 @@
 #define COND_DIRECT_INSTRUCTION_H
 
 #include "instruction.h"
+#include "checkpoint.h"
+
+typedef std::set<ADDRINT>                         addrint_set;
+typedef std::map<ADDRINT, UINT8>                  addrint_value;
+typedef std::set<addrint_value>                   addrint_value_set;
+typedef std::pair<ptr_checkpoint_t, addrint_set>  checkpoint_with_modified_addrs;
 
 class cond_direct_instruction : public instruction
 {
+public:
+  bool is_resolved;
+  bool is_bypassed;
+  bool is_explored;
+
+  addrint_set                                 input_dep_addrs;
+  std::map<bool, addrint_value_set>           inputs;
+  std::vector<checkpoint_with_modified_addrs> checkpoints;
+
 public:
   cond_direct_instruction(const INS& ins);
 };
