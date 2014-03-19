ce59b18d59736b2fa4db2ca8063660934c9cee0f
Ta Thanh Dinh <tathanhdinh@gmail.com>
2014-03-19 01:50:44 +0100

    use funtion_traits to extract params for wrapper functions

--- version_1/src/operation/capturing_phase.h
+++ version_1/src/operation/capturing_phase.h
@@ -23,16 +23,24 @@
 namespace windows
 {
 #define NOMINMAX
+#define INCL_WINSOCK_API_PROTOTYPES 1
+#define INCL_WINSOCK_API_TYPEDEFS   1
 #include <WinSock2.h>
 #include <Windows.h>
+
+typedef int recv_t(SOCKET, char*, int, int);
+typedef int recvfrom_t(SOCKET, char*, int, int, struct sockaddr*, int*);
 };
 #endif
+
+#include <boost/type_traits.hpp>
 
 namespace capturing
 {
 extern auto initialize                  ()                                                -> void;
 
 #if defined(_WIN32) || defined(_WIN64)
+
 extern auto recvs_interceptor_before    (ADDRINT msg_addr, THREADID thread_id)            -> VOID;
 
 extern auto recvs_interceptor_after     (UINT32 msg_length, THREADID thread_id)           -> VOID;
@@ -41,21 +49,23 @@ extern auto wsarecvs_interceptor_before (ADDRINT msg_struct_addr, THREADID threa
 
 extern auto wsarecvs_interceptor_after  (THREADID thread_id)                              -> VOID;
 
+typedef boost::function_traits<windows::recv_t> recv_traits;
 extern auto recv_wrapper                (AFUNPTR recv_origin,
-                                         windows::SOCKET s,
-                                         char* buf,
-                                         int len,
-                                         int flags,
-                                         CONTEXT* p_ctxt, THREADID thread_id)             -> int;
+                                         /*windows::SOCKET*/recv_traits::arg1_type s,
+                                         /*char**/recv_traits::arg2_type buf,
+                                         /*int*/recv_traits::arg3_type len,
+                                         /*int*/recv_traits::arg4_type flags,
+                                         CONTEXT* p_ctxt, THREADID thread_id)             -> recv_traits::result_type;
 
+typedef boost::function_traits<windows::recvfrom_t> recvfrom_traits;
 extern auto recvfrom_wrapper            (AFUNPTR recvfrom_origin,
-                                         windows::SOCKET s,
-                                         char* buf,
-                                         int len,
-                                         int flags,
-                                         windows::sockaddr* from,
-                                         int* fromlen,
-                                         CONTEXT* p_ctxt, THREADID thread_id)              -> int;
+                                         /*windows::SOCKET*/recvfrom_traits::arg1_type s,
+                                         /*char**/recvfrom_traits::arg2_type buf,
+                                         /*int*/recvfrom_traits::arg3_type len,
+                                         /*int*/recvfrom_traits::arg4_type flags,
+                                         /*windows::sockaddr**/recvfrom_traits::arg5_type from,
+                                         /*int**/recvfrom_traits::arg6_type fromlen,
+                                         CONTEXT* p_ctxt, THREADID thread_id)              -> recvfrom_traits::result_type;
 
 extern auto wsarecv_wrapper             (AFUNPTR wsarecv_origin,
                                          windows::SOCKET s,
