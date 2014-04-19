976a9f0cfad280d659f2b9956e9eebc6c52ac08a
Ta Thanh Dinh <tathanhdinh@gmail.com>
2014-04-19 20:17:23 +0200

    fixed point implementation for path condition

--- version_1/src/base/execution_path.cpp
+++ version_1/src/base/execution_path.cpp
@@ -1,14 +1,67 @@
 #include "execution_path.h"
 
+#include <functional>
 
 /**
  * @brief calculate join (least upper bound or cartesian product) of two cfi conditions
  */
-addrint_value_maps_t join_conditions(addrint_value_maps_t cond_a, addrint_value_maps_t cond_b)
+addrint_value_maps_t join_conditions(const addrint_value_maps_t& cond_a,
+                                     const addrint_value_maps_t& cond_b)
 {
   addrint_value_maps_t joined_cond;
 
+  std::for_each(cond_a.begin(), cond_a.end(), [&](addrint_value_maps_t::value_type a_map)
+  {
+    std::for_each(cond_b.begin(), cond_b.end(), [&](addrint_value_maps_t::value_type b_map)
+    {
+      addrint_value_map_t joined_map = a_map;
+      bool is_conflict = false;
+
+      std::for_each(b_map.begin(), b_map.end(),
+                    [&](addrint_value_maps_t::value_type::value_type b_point)
+      {
+        if (!is_conflict)
+        {
+          // verify if the source of b_point exists in the a_map
+          if (a_map.find(b_point.first) != a_map.end())
+          {
+            // exists, then verify if there is a conflict between corresponding targets
+            // in a_map and b_map
+            if (a_map[b_point.first] != b_map[b_point.first])
+            {
+              is_conflict = true;
+            }
+          }
+          else
+          {
+            // does not exist, then add b_point into the joined map
+            joined_map.insert(b_point);
+          }
+        }
+      });
+
+      if (!is_conflict) joined_cond.push_back(joined_map);
+    });
+  });
+
   return joined_cond;
+}
+
+
+/**
+ * @brief join_if_needed
+ */
+condition_t join_if_needed(condition_t in_cond)
+{
+  condition_t out_cond;
+  return out_cond;
+}
+
+
+condition_t fix(std::function<decltype(join_if_needed)> join_func)
+{
+//  return join_func(fix(join_func));
+  return std::bind(join_func, std::bind(&fix, join_func), std::placeholders::_1);
 }
 
 
--- version_1/src/operation/common.h
+++ version_1/src/operation/common.h
@@ -1,22 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-// these definitions are not necessary (defined already in the CMakeLists),
-// they are added just to help qt-creator parsing headers
-#if defined(_WIN32) || defined(_WIN64)
-#ifndef TARGET_IA32
-#define TARGET_IA32
-#endif
-#ifndef HOST_IA32
-#define HOST_IA32
-#endif
-#ifndef TARGET_WINDOWS
-#define TARGET_WINDOWS
-#endif
-#ifndef USING_XED
-#define USING_XED
-#endif
-#endif
+#include "../parsing_helper.h"
 #include <pin.H>
 
 #include <fstream>
